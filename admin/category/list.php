e90cd1afd4644a6249caa7bba75b7dc60dd890d4
Luanptph42636 <149581561+Luanptph42636@users.noreply.github.com>
2023-11-25 16:35:58 +0700

    giao dien chua hoan thien

--- admin/category/list.php
+++ admin/category/list.php
@@ -26,7 +26,7 @@
                     width: 800px;
                 }
                 .btn {
-                    margin-left: -50px;
+                    margin-left: -58px;
                 }
                 .card,.table-responsive{
                     width: 850px;
@@ -41,38 +41,6 @@
                 <div class="card-body">
                     <div class="d-md-flex">
                         <div class="d-none d-md-flex">All Category</div>
-                        <!-- <div class="d-md-flex gap-4 align-items-center">
-                            <form class="mb-3 mb-md-0">
-                                <div class="row g-3">
-                                    <div class="col-md-6">
-                                        <select class="form-select">
-                                            <option>Sort by</option>
-                                            <option value="desc">Desc</option>
-                                            <option value="asc">Asc</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <select class="form-select">
-                                            <option value="10">10</option>
-                                            <option value="20">20</option>
-                                            <option value="30">30</option>
-                                            <option value="40">40</option> 
-                                            <option value="50">50</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </form>
-                        </div> -->
-                        <!-- <div class="dropdown ms-auto">
-                            <a href="#" data-bs-toggle="dropdown"
-                               class="btn btn-primary dropdown-toggle"
-                               aria-haspopup="true" aria-expanded="false">Actions</a>
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <a href="#" class="dropdown-item">Action</a>
-                                <a href="#" class="dropdown-item">Another action</a>
-                                <a href="#" class="dropdown-item">Something else here</a>
-                            </div>
-                        </div> -->
                     </div>
                 </div>
             </div>
--- admin/product/list.php
+++ admin/product/list.php
@@ -4,155 +4,7 @@
     }
 ?>
 <div class="content ">
-    <div class="row">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                    <div class="d-md-flex gap-4 align-items-center">
-                        <div class="d-none d-md-flex">All Products</div>
-                        <!-- <div class="d-md-flex gap-4 align-items-center">
-                            <form class="mb-3 mb-md-0">
-                                <div class="row g-3">
-                                    <div class="col-md-6">
-                                        <select class="form-select" name="arrange">
-                                            <option>Sort by</option
-                                             <option value="desc">Desc</option>
-                                            <option value="asc">Asc</option> -->
-                                        <!-- </select>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <select class="form-select">
-                                            <option value="10">10</option>
-                                            <option value="20">20</option>
-                                            <option value="30">30</option>
-                                            <option value="40">40</option>
-                                            <option value="50">50</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>  -->
-                        <!-- <div class="dropdown ms-auto">
-                            <a href="#" data-bs-toggle="dropdown"
-                               class="btn btn-primary dropdown-toggle"
-                               aria-haspopup="true" aria-expanded="false">Actions</a>
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <a href="#" class="dropdown-item">Action</a>
-                                <a href="#" class="dropdown-item">Another action</a>
-                                <a href="#" class="dropdown-item">Something else here</a>
-                            </div>
-                        </div> -->
-                    </div>
-                </div>
-            </div>
-            <div class="table-responsive">
-                <table class="table table-custom table-lg mb-0" id="products">
-                    <thead>
-                    <tr>
-                        <!-- <th>
-                            <input class="form-check-input select-all" type="checkbox"
-                                   data-select-all-target="#products" id="defaultCheck1">
-                        </th> -->
-                        <th>ID</th>
-                        <th>Photo</th>
-                        <th>Name</th>
-                        <th>Stock</th>
-                        <th>Price</th>
-                        <th>Created At</th>
-                        <th class="text-end">Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        
-                        <?php
-                        foreach($list_pro as $pd){
-                            extract($pd);
-                            $product_image=load_image($product_id);
-                            extract($product_image);
-                            $edit_product="index.php?act=edit_product&product_id=".$product_id;
-                            $delete_product="index.php?act=delete_product&product_id=".$product_id;
-                            $show_detail="index.php?act=product_detail&product_id=".$product_id;
-                            $image="../upload/".$image_name;    
-                            ?>
-                                    <tr>
-                                        <td>
-                                            <a href="#">#<?=$product_id?></a>
-                                        </td>
-                                        <td>
-                                            <a href="#">
-                                                <img src="<?=$image?>" class="rounded" width="40" alt="">
-                                            </a>
-                                        </td>
-                                        <td><?=$product_name;?></td>
-                                        <?php if($status==1){?>
-                                           <td>
-                                           <span class="text-success">In Stock</span>
-                                           </td>
-                                           <?php } ?>
-                                        <?php if($status== 0){?>
-                                            <td style="color:red;"><span>Out Stock</span></td>
-                                        <?php } ?>
-                                            <td><?=$product_price?></td>
-                                            <td><?=$date_add?></td>
-                                            <td class="text-end"><a href="<?=$edit_product?>"><button>EDIT</button></a> | 
-                                            <a href="<?=$delete_product?>"><button>DELETE</button></a> | 
-                                            <a href="<?= $show_detail?>"><button>DETAIL</button></a>
-                                            </td>
-                                            </tr>
-                        <?php
-                        }
-                        ?>
-                    </tbody>
-                </table>
-               
-                    
-                    
-                   
-                
-            </div>
-            <form action="index.php?act=list_product" method="post">
-            <nav class="mt-4" aria-label="Page navigation example">
-                <ul class="pagination justify-content-center">
-                    <!-- <li class="page-item">
-                        <a class="page-link" href="#" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
-                    </li> -->
-                    <!-- <li class="page-item"><input type="submit" name="number"  class="page" value="1"></a></li>
-                    <li class="page-item"><input type="submit" name="number"  class="page" value="2"></a></li>
-                    <li class="page-item"><input type="submit" name="number" class="page" value="3"></a></li> -->
-                    <!-- <li class="page-item">
-                        <a class="page-link" href="#" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                        </a>
-                    </li> -->
-                    <?php
-                        for ($i=0; $i < $count; $i++) { 
-                    ?>
-                        <input type="submit" name="number" class="page"value="<?=$i+1?>">
-                    <?php
-                        }
-                    ?>
-                    <style>
-                        .page{
-                            width: 30px;
-                            height: 30px;
-                            margin: 5px;
-                            color:#ff6e40 ;
-                            border:solid 1px #ff6e40 ;
-                            background-color: #fff;
-                            border-radius: 10px;
-                        }
-                        .page:hover{
-                            background-color:#ff6e40;
-                            color: #fff;
-                        }
-                    </style>
-                </ul>
-            </nav>
-            </form>
-        </div>
-        <div class="col-md-4">
+<div class="col-md-4">
             <h5 class="mb-4">Filter Products</h5>
             <div class="card mb-4">
                 <div class="card-body">
@@ -258,6 +110,115 @@
                 </div>
             </div> -->
         </div>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-md-flex gap-4 align-items-center">
+                        <div class="d-none d-md-flex">All Products</div>
+                    </div>
+                </div>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-custom table-lg mb-0" id="products">
+                    <thead>
+                    <tr>
+                        <!-- <th>
+                            <input class="form-check-input select-all" type="checkbox"
+                                   data-select-all-target="#products" id="defaultCheck1">
+                        </th> -->
+                        <th>ID</th>
+                        <th>Photo</th>
+                        <th>Name</th>
+                        <!-- <th>Stock</th> -->
+                        <th>Price</th>
+                        <th>Created At</th>
+                        <th class="text-end">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        
+                        <?php
+                        foreach($list_pro as $pd){
+                            extract($pd);
+                            $product_image=load_image($product_id);
+                            extract($product_image);
+                            $edit_product="index.php?act=edit_product&product_id=".$product_id;
+                            $delete_product="index.php?act=delete_product&product_id=".$product_id;
+                            $show_detail="index.php?act=product_detail&product_id=".$product_id;
+                            $image="../upload/".$image_name;    
+                            ?>
+                                    <tr>
+                                        <td>
+                                            <a href="#">#<?=$product_id?></a>
+                                        </td>
+                                        <td>
+                                            <a href="#">
+                                                <img src="<?=$image?>" class="rounded" width="40" alt="">
+                                            </a>
+                                        </td>
+                                        <td><?=$product_name;?></td>
+                                            <td><?=$product_price?></td>
+                                            <td><?=$date_add?></td>
+                                            <td class="text-end"><a href="<?=$edit_product?>"><button>EDIT</button></a> | 
+                                            <a href="<?=$delete_product?>"><button>DELETE</button></a> | 
+                                            <a href="<?= $show_detail?>"><button>DETAIL</button></a>
+                                            </td>
+                                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+               
+                    
+                    
+                   
+                
+            </div>
+            <form action="index.php?act=list_product" method="post">
+            <nav class="mt-4" aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <!-- <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li> -->
+                    <!-- <li class="page-item"><input type="submit" name="number"  class="page" value="1"></a></li>
+                    <li class="page-item"><input type="submit" name="number"  class="page" value="2"></a></li>
+                    <li class="page-item"><input type="submit" name="number" class="page" value="3"></a></li> -->
+                    <!-- <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li> -->
+                    <?php
+                        for ($i=0; $i < $count; $i++) { 
+                    ?>
+                        <input type="submit" name="number" class="page"value="<?=$i+1?>">
+                    <?php
+                        }
+                    ?>
+                    <style>
+                        .page{
+                            width: 30px;
+                            height: 30px;
+                            margin: 5px;
+                            color:#ff6e40 ;
+                            border:solid 1px #ff6e40 ;
+                            background-color: #fff;
+                            border-radius: 10px;
+                        }
+                        .page:hover{
+                            background-color:#ff6e40;
+                            color: #fff;
+                        }
+                    </style>
+                </ul>
+            </nav>
+            </form>
+        </div>
+
     </div>
 
     </div>
